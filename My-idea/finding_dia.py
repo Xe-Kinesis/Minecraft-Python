296cabb82ade77cf2ac4bd7439ed73005ea76f70
Kinesis <lordkinesis@naver.com>
2021-09-20 13:47:00 +0900

    Add

---
+++ My-idea/finding_dia.py
@@ -0,0 +1,18 @@
+'''
+다이아 감지 봇
+알고리즘
+'''
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+
+x, y, z = mc.player.getPos()
+
+while True:
+    for i in range(0, 50):
+        block_finder = mc.getBlock(x, y-i, z)
+        if block_finder == 56:
+            mc.postToChat("Founded The Dia! y : -" + str(i))
+        else:
+            mc.postToChat("No dia here" + str(i))
+            time.sleep(1)
