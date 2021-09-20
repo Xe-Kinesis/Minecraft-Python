d3cb865d49532c43b9d7d683b3ec0fdbc4ac9745
Kinesis <lordkinesis@naver.com>
2021-09-20 16:15:49 +0900

    Modify the finding_dia.py

--- Abillity/finding_dia.py
+++ Abillity/finding_dia.py
@@ -1,18 +1,37 @@
 '''
 다이아 감지 봇
-알고리즘 .
 '''
+#환경 설정
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 import time
 
+#bag는 다이아 감지 저장 통
+bag = []
 x, y, z = mc.player.getPos()
 
-while True:
-    for i in range(0, 50):
-        block_finder = mc.getBlock(x, y-i, z)
-        if block_finder == 56:
-            mc.postToChat("Founded The Dia! y : -" + str(i))
-        else:
-            mc.postToChat("No dia here" + str(i))
-            time.sleep(1)
+mc.postToChat("Fiding now..")
+
+#y축 최대 감지 범위 : 69칸(변경 가능)
+for i in range(0, 70):
+    block_finder = mc.getBlock(x, y-i, z)
+    bag.append(block_finder)
+
+
+#인덱스 중복값 출력
+fin_bag = [i for i, value in enumerate(bag) if value == 56]
+mc.postToChat("Done!")
+time.sleep(1)
+
+if 56 in bag:
+    mc.postToChat("==========")       
+    mc.postToChat("Found the location of the diamond!")
+    mc.postToChat("Total Count : " + str(bag.count(56)))
+    mc.postToChat("The diamond is located below the " + str(fin_bag) + "column on the y-axis.")
+    mc.postToChat("==========")    
+else:
+    mc.postToChat("No Diamond here T.T")
+
+#리스트 초기화
+bag.clear()
+fin_bag.clear()
